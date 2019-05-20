f2eb937dbfdfbcc6f3882bee97326f519242a3d7
mdraza <raza.uidev@gmail.com>
2019-05-20 12:31:11 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.view-img').magnificPopup({
+    type: 'image',
+    gallery: {
+        enabled: true,
+    },
+    zoom: {
+        enabled: true,
+        duration: 300,
+        easing: 'ease-in-out'
+    }
+  });
